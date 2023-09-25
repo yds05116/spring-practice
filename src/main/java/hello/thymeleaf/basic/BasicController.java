93ab3427d42de8f162e58e7c77a05279effa6ca8
yds05116 <yds05116@naver.com>
2023-09-25 21:11:15 +0900

    인라인 함수부터 듣자

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -57,6 +58,55 @@ public class BasicController {
         model.addAttribute("servletContext", request.getServletContext());
         return "basic/basic-objects";
     }
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("LocalDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+    @GetMapping("/link")
+    public String link(Model model){
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+    @GetMapping("/literal")
+    public String literal(Model model){
+        model.addAttribute("data", "spring!");
+        return "basic/literal";
+    }
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "spring!");
+        return "basic/operation";
+    }
+    @GetMapping("/attribute")
+    public String attribute(Model model){
+        return "basic/attribute";
+    }
+    @GetMapping("/each")
+    public String each(Model model){
+        addUsers(model);
+        return "basic/each";
+    }
+    @GetMapping("/comments")
+    public String comments(Model model){
+        model.addAttribute("data","Spring");
+        return "basic/comments";
+    }
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
+    private void addUsers(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users",list);
+    }
+
     @Component("helloBean")
     static class HelloBean { // 객체 생성 후, 스프링 빈 등록
         public String hello(String data) {
